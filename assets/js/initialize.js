aae6806213c739b17c653b3ba9a5056a1257b822
Louis <louis.masson@palissy.fr>
2019-01-31 20:57:41 +0100

    :art: Improving structure of files and css

---
+++ assets/js/initialize.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let canvas;
+
+let imgData;
+
+let img;
+
+let animationTimeout;
+
+window.onload = init;
+
+function init() {
+	canvas = document.querySelector("canvas");
+	setTargetContext(canvas.getContext("2d"));
+
+	img = new Image();
+
+	img.src = "assets/img/link.jpg";
+	img.onload = draw;
+}
+
+function draw() {
+	canvas.width = img.width;
+	canvas.height = img.height
+	drawImage(img, 0, 0, img.width, img.height);
+
+	imgData = getImageData(0, 0, canvas.width, canvas.height); 
+
+	filterImageData(imgData, FilterType.SEPIA);
+	putImageData(imgData, 0, 0);
+}
