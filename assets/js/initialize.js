b20ec85548cc1aed781bf9bc37d79ab14355fc91
Louis <louis.masson@palissy.fr>
2019-01-31 22:29:07 +0100

    :construction: Add handling of the dimensions of the image

--- assets/js/initialize.js
+++ assets/js/initialize.js
@@ -21,6 +21,9 @@ function init() {
 }
 
 function draw() {
+	console.log(img.width, img.height)
+	initImg();
+
 	canvas.width = img.width;
 	canvas.height = img.height
 	drawImage(img, 0, 0, img.width, img.height);
@@ -29,4 +32,16 @@ function draw() {
 
 	filterImageData(imgData, FilterType.SEPIA);
 	putImageData(imgData, 0, 0);
+}
+
+function initImg() {
+	let ratio = img.height/img.width;
+
+	if(img.height < img.width) {
+		img.width = 700;
+		img.height = Math.floor(ratio*img.width);
+	} else {
+		img.height = 433;
+		img.width = Math.floor(ratio*img.height)
+	}
 }
